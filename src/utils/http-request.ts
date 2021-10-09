428cc5b232671f946935ab6c565793f7e2737034
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-10-08 22:36:39 -0400

    refactor

--- src/utils/http-request.ts
+++ src/utils/http-request.ts
@@ -3,13 +3,20 @@ import * as http from 'http';
 
 const httpRequest = async (alpha2Code) => {
   const url = `http://api.countrylayer.com/v2/lang/${alpha2Code}?access_key=${process.env.COUNTRY_LAYER_API_KEY}`;
-  const response: { data: any[] } = { data: [] };
+  const response: { data: object } = { data: [] };
   return await new Promise<typeof response>((resolve, reject) =>
     http
       .get(url, (res) => {
+        let data: string = '';
         if (res.statusCode === 200) {
-          res.on('data', (chunk) => response.data.push(JSON.parse(chunk)));
-          res.on('end', () => resolve(response));
+          res.on('data', (chunk) => {
+            data += chunk;
+          });
+          res.on('end', () => {
+            const json = JSON.parse(data);
+            response.data = json;
+            resolve(response);
+          });
         }
       })
       .on('error', (err) => {
